4be430a91c05bdf76e871ed46b75351640a34b85
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-01 10:38:03 +0530

    Initial commit

---
+++ app/src/main/java/e/wolfsoft1/signup_i_know_uikit/MainActivity.java
@@ -0,0 +1,22 @@
+package e.wolfsoft1.signup_i_know_uikit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.next_main);
+
+
+
+
+    }
+}
